31ff761b0fc252aad574405cd5ee3a1db68c41d0
reallyquiet <yangjm2067@sina.com>
2017-10-07 14:18:44 +0800

    Learn and,or,ASCII.

---
+++ aToA.asm
@@ -0,0 +1,36 @@
+assume cs:code,ds:data
+
+data segment
+	db 'BaSiC'
+	db 'inFORmaTION'
+data ends
+
+code segment
+start:mov ax, data
+	mov ds, ax
+
+	mov bx, 0; ds:bx => 'BaSiC'的第一个字母
+
+	mov cx, 5; 循环次数
+s:  mov al, [bx]
+	and al, 11011111B; 变大写
+	mov [bx], al
+
+	inc bx
+	loop s
+
+
+	mov bx, 5; ds:bx =>'inFORmaTION'的第一个字母
+
+	mov cx, 11
+s0: mov al, [bx]
+	or al, 00100000B; 变小写
+	mov [bx], al
+	inc bx
+	loop s0
+
+	mov ax, 4c00H
+	int 21H
+
+code ends
+end sart
