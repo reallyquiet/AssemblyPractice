a25a71f175b1e33d0529f5b7646a48021318b80e
reallyquiet <yangjm2067@sina.com>
2017-10-08 22:42:10 +0800

    Update aToA.asm

--- aToA.asm
+++ aToA.asm
@@ -12,7 +12,7 @@ start:mov ax, data
 	mov bx, 0; ds:bx => 'BaSiC'的第一个字母
 
 	mov cx, 5; 循环次数
-s:  mov al, [bx]
+    s:  mov al, [bx]
 	and al, 11011111B; 变大写
 	mov [bx], al
 
@@ -23,14 +23,15 @@ s:  mov al, [bx]
 	mov bx, 5; ds:bx =>'inFORmaTION'的第一个字母
 
 	mov cx, 11
-s0: mov al, [bx]
+    s0: mov al, [bx]
 	or al, 00100000B; 变小写
 	mov [bx], al
+	
 	inc bx
 	loop s0
 
 	mov ax, 4c00H
 	int 21H
-
+	
 code ends
-end sart
+end start
